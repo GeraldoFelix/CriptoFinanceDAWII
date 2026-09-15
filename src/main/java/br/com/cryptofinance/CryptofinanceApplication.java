04952ca7c0e4ee08310577cb6f1d2ff86af5b31b
Geraldo Felix Jr <geraldosilvajr06@gmail.com>
2026-09-15 16:12:41 -0300

    SpringInitializr

---
+++ src/main/java/br/com/cryptofinance/CryptofinanceApplication.java
@@ -0,0 +1,13 @@
+package br.com.cryptofinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptofinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptofinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cryptofinance/CryptofinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cryptofinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptofinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
